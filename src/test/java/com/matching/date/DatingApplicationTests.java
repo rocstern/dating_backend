45f7e92b121860d4e859f026d938e3e3b7fab6eb
rocstern <rocstern1@gmail.com>
2021-07-06 11:08:53 +0900

    first commit

---
+++ src/test/java/com/matching/date/DatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matching.date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
